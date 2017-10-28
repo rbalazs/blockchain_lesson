9ec4c623c04e666f67265ef78b4df1f586018a37
Richer Balázs <richer.balazs@gmail.com>
2017-10-28 13:41:53 +0200

    Fill new_block method with logic.

--- blockchain.py
+++ blockchain.py
@@ -2,12 +2,31 @@ class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transaction = []
+        self.new_block(previus_hash=1, proof=100)
 
-    def new_block(self):
-        pass
+    def new_block(self, proof, previus_hash=None):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transaction,
+            'proof': proof,
+            'previus_hash': previus_hash or self.hash(self.chain[-1])
+        }
+
+        self.current_transaction = []
+
+        self.chain.append(block)
+
+        return block
 
     def new_transaction(self, sender, recipient, amount):
-        pass
+        self.current_transaction.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount
+        })
+
+        return self.last_block['index'] + 1
 
     @staticmethod
     def hash(block):
