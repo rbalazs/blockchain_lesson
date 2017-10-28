e3cbdbd8f0078c2a39ea087c8b5c68de71aa5804
Richer Balázs <richer.balazs@gmail.com>
2017-10-28 15:12:04 +0200

    Minor typos and bugfixes III.

--- blockchain.py
+++ blockchain.py
@@ -98,11 +98,11 @@ def mine():
 def new_transaction():
     values = request.get_json()
 
-    required = ['sender', 'recepient', 'amount']
+    required = ['sender', 'recipient', 'amount']
     if not all(k in values for k in required):
         return 'Missing values', 400
 
-    index = blockchain.new_transaction(values['sender'], values['recepient'], values['amount'])
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
 
     response = {'message': f'Transaction will be added to Block {index}'}
     return jsonify(response), 201
