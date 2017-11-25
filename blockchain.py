ac2a3e3f5a124d27e923a1a6ec61a52403fef503
Richer Balázs <richer.balazs@gmail.com>
2017-11-25 10:31:59 +0100

    Method new_block creates a block with the give proof, and stores the current transaction into the blockchain.

--- blockchain.py
+++ blockchain.py
@@ -3,8 +3,20 @@ class Blockchain(object):
         self.chain = []
         self.current_transactions = []
 
-    def new_block(self):
-        pass
+    def new_block(self, proof, previous_hash=None):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1])
+        }
+
+        self.current_transactions = []
+
+        self.chain.append(block)
+
+        return block
 
     def new_transactions(self, sender, recipient, amount):
         self.current_transactions.append({
