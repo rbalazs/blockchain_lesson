46fa165951c55d7b91e7eef5ea941b4fd3354a17
Richer Balázs <richer.balazs@gmail.com>
2017-11-25 10:46:52 +0100

    Implement Proof Of Work algorithm to verify hashcash.

--- blockchain.py
+++ blockchain.py
@@ -41,3 +41,15 @@ class Blockchain(object):
     def last_block(self):
         pass
 
+    def proof_of_work(self, last_proof):
+        proof = 0
+        while (self.valid_proof(last_proof, proof)) is False:
+            proof += 1
+
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
