8a27ae3cca7e87de90d0a1290d6271719b6863dd
Richer Balázs <richer.balazs@gmail.com>
2017-12-16 19:57:10 +0100

    Method to download other ledgers, verify them, and pick the longest one (Consensus Algorithm)

--- blockchain.py
+++ blockchain.py
@@ -38,6 +38,26 @@ class Blockchain(object):
 
         return True
 
+    def resolve_conflicts(self):
+        neighbours = self.nodes
+        new_chain = None
+        max_length = len(self.chain)
+
+        for node in neighbours:
+            response = request.get(f'http://{node}/chain')
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+        if new_chain:
+            self.chain = new_chain
+            return True
+
+        return False
+
     def new_block(self, proof, previous_hash=None):
         block = {
             'index': len(self.chain) + 1,
